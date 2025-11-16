599c7b1ebdf4613f56dc9a457178be8f8ceb34a6
erstwld <43255124+erstwld@users.noreply.github.com>
2025-11-15 23:46:54 -0500

    navigation bar working for all pages

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,13 @@
 import "../app.css";
+import { Link } from "react-router";
 
 export function Header() {
   return (
     <nav className="navbar">
-      <a href="/aboutme">About Me</a>
-      <a href="/education">Education</a>
-      <a href="/experience">Experience</a>
-      <a href="/contact">Contact</a>
+      <Link to="/">About Me</Link>
+      <Link to="/education">Education</Link>
+      <Link to="/experience">Experience</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   );
 }
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/aboutme.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/aboutme.tsx"), 
+    route("education", "routes/education.tsx"),
+    route("contact", "routes/contact.tsx"),
+    route("experience", "routes/experience.tsx")
+] satisfies RouteConfig;
